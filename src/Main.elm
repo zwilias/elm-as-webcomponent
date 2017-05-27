945756bdcd1bb9f7c24fe2395d1fc1b2850432ed
ilias <ilias.van.peer@telenetgroup.be>
2017-05-27 16:30:03 +0200

    Add attribute support

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,38 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (Html, programWithFlags, text)
+import Json.Decode as Json exposing (Value, Decoder)
 
 
-main : Html a
+type alias Model =
+    { name : String }
+
+
+init : Value -> ( Model, Cmd msg )
+init flags =
+    case Json.decodeValue (Json.field "name" Json.string) flags of
+        Ok name ->
+            Model name ! []
+
+        Err _ ->
+            Model "no one" ! []
+
+
+update : msg -> Model -> ( Model, Cmd msg )
+update _ model =
+    model ! []
+
+
+view : Model -> Html msg
+view { name } =
+    text <| "Hello, " ++ name ++ "!"
+
+
+main : Program Value Model msg
 main =
-    text "Hello world!"
+    programWithFlags
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
